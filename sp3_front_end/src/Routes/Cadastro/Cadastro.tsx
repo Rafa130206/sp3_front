59ae2438a63cb4482a5b211aa739a9a41484b20b
Rafa130206 <rafabompadre@gmail.com>
2024-09-13 01:53:59 -0300

    Página de cadastro criada.

--- sp3_front_end/src/Routes/Cadastro/Cadastro.tsx
+++ sp3_front_end/src/Routes/Cadastro/Cadastro.tsx
@@ -1,5 +1,63 @@
+import './Cadastro.css'
+
 export default function Cadastro() {
+
     return(
-        <h1>Cadastro</h1>
+        <div>
+            <h1>Cadastro</h1>
+            <div className='forms'>
+            <form>
+            <fieldset>
+                    <legend>Usuário</legend>
+                    <label>
+                        <input type="text" placeholder='Insira seu Nome'/>
+                    </label>
+                    <label>
+                        <input type='text' placeholder='Insira seu CEP'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira seu CPF'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira seu RG'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira seu Telefone'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira seu Email'/>
+                    </label>
+                </fieldset>
+
+                <button type='submit'>Cadastrar Usuário</button>
+            </form>
+                
+            <form>
+                <fieldset>
+                    <legend>Veículo</legend>
+                    <label>
+                        <input type="text" placeholder='Insira a placa do veículo'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira a placa do veículo'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira o modelo do veículo'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira o tipo do veículo'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira o ano do veículo'/>
+                    </label>
+                    <label>
+                        <input type="text" placeholder='Insira a cor do veículo'/>
+                    </label>
+                </fieldset>
+
+                <button type='submit'>Cadastrar Veículo</button>
+            </form>
+            </div>
+        </div>
     )
 }
