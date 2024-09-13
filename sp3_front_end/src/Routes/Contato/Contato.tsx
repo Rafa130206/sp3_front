4a62bd6388d52598dc35462f8755e58ec6cabd3f
Rafa130206 <rafabompadre@gmail.com>
2024-09-13 02:22:48 -0300

    Página de integrantes criada e estilizada.

--- sp3_front_end/src/Routes/Contato/Contato.tsx
+++ sp3_front_end/src/Routes/Contato/Contato.tsx
@@ -1,5 +1,26 @@
+import './Contato.css'
+
 export default function Contato() {
     return(
-        <h1>Contato</h1>
+        <div>
+            <h1 className='cadastroh1'>Entre em contato conosco!</h1>
+            <section className='form'>
+                <form action="Cadastrar" method='get'>
+                    <fieldset>
+                        <legend>Dados Pessoais</legend><br />
+                        <label>
+                            <input type="text" name="nome" id="idnome" placeholder='Insira seu nome' />
+                        </label>
+                        <br />
+                        <label>
+                            <input type="text" name='email' id='idemail' placeholder='Insira seu email'/>
+                        </label>
+                        <label>
+                            <input type="text" name='mensagem'/>
+                        </label>
+                    </fieldset>
+                </form>
+            </section>
+        </div>
     )
 }
